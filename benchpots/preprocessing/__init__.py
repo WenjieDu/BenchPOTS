964eac67d60215e97450a4de04fe8ff7e7a6c3fe
Wenjie Du <wenjay.du@gmail.com>
2024-05-16 23:20:17 +0800

    feat: update preprocess_physionet2012();

--- benchpots/preprocessing/__init__.py
+++ benchpots/preprocessing/__init__.py
@@ -4,3 +4,10 @@
 
 # Created by Wenjie Du <wenjay.du@gmail.com>
 # License: BSD-3-Clause
+
+from physionet_2012 import preprocess_physionet2012
+
+
+__all__ = [
+    "preprocess_physionet2012",
+]
--- benchpots/preprocessing/physionet_2012.py
+++ benchpots/preprocessing/physionet_2012.py
@@ -17,12 +17,8 @@ def preprocess_physionet2012():
 
     Returns
     -------
-    data: dict,
-        A dictionary containing the generated PhysioNet-2012 dataset.
-            X : pandas.DataFrame,
-                A dataframe contains all time series vectors from 11988 patients, distinguished by column `RecordID`.
-            y : pandas.Series
-                The 11988 classification labels of all patients, indicating whether they were deceased.
+    processed_dataset: dict,
+        A dictionary containing the processed PhysioNet-2012 dataset.
 
     """
 
@@ -48,7 +44,7 @@ def preprocess_physionet2012():
     X = X.drop(["level_1", "ICUType"], axis=1)
     y = data["y"]
 
-    # PhysioNet2012 is an imbalanced dataset, hence, separate positive and negative samples here for later splitting.
+    # PhysioNet2012 is an imbalanced dataset, hence, we separate positive and negative samples here for later splitting
     # This is to ensure positive and negative ratios are similar in train/val/test sets
     all_recordID = X["RecordID"].unique()
     positive = (y == 1)["In-hospital_death"]
@@ -100,11 +96,13 @@ def preprocess_physionet2012():
     val_X = val_X.reshape(len(val_set_ids), 48, -1)
     test_X = test_X.reshape(len(test_set_ids), 48, -1)
 
+    # fetch labels for train/val/test sets
     train_y = y[y.index.isin(train_set_ids)].sort_index()
     val_y = y[y.index.isin(val_set_ids)].sort_index()
     test_y = y[y.index.isin(test_set_ids)].sort_index()
     train_y, val_y, test_y = train_y.to_numpy(), val_y.to_numpy(), test_y.to_numpy()
 
+    # fetch ICUType for train/val/test sets
     train_ICUType = ICUType[ICUType.index.isin(train_set_ids)].sort_index()
     val_ICUType = ICUType[ICUType.index.isin(val_set_ids)].sort_index()
     test_ICUType = ICUType[ICUType.index.isin(test_set_ids)].sort_index()
@@ -114,20 +112,25 @@ def preprocess_physionet2012():
         test_ICUType.to_numpy(),
     )
 
-    processed_data_set = {
+    # assemble the final processed data into a dictionary
+    processed_dataset = {
+        # general info
         "n_classes": 2,
         "n_steps": 48,
         "n_features": train_X.shape[-1],
+        "scaler": scaler,
+        # train set
         "train_X": train_X,
         "train_y": train_y.flatten(),
         "train_ICUType": train_ICUType.flatten(),
+        # val set
         "val_X": val_X,
         "val_y": val_y.flatten(),
         "val_ICUType": val_ICUType.flatten(),
+        # test set
         "test_X": test_X,
         "test_y": test_y.flatten(),
         "test_ICUType": test_ICUType.flatten(),
-        "scaler": scaler,
     }
 
-    return processed_data_set
+    return processed_dataset
