5eae2ce2c9b848b94df2daaf36423d50b5898c2c
Wenjie Du <wenjay.du@gmail.com>
2024-05-24 23:54:54 +0800

    feat: add preprocess_pems_traffic;

--- benchpots/preprocessing/__init__.py
+++ benchpots/preprocessing/__init__.py
@@ -8,6 +8,7 @@
 from .beijing_multisite_air_quality import preprocess_beijing_air_quality
 from .electricity_load_diagrams import preprocess_electricity_load_diagrams
 from .electricity_transformer_temperature import preprocess_ett
+from .pems_traffic import preprocess_pems_traffic
 from .physionet_2012 import preprocess_physionet2012
 
 __all__ = [
@@ -15,4 +16,5 @@ __all__ = [
     "preprocess_beijing_air_quality",
     "preprocess_electricity_load_diagrams",
     "preprocess_ett",
+    "preprocess_pems_traffic",
 ]
