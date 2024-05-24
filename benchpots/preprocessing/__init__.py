7656a246fab24ba4e1531e1a43b565068bbb4676
Wenjie Du <wenjay.du@gmail.com>
2024-05-24 23:51:53 +0800

    feat: add preprocess_ett;

--- benchpots/preprocessing/__init__.py
+++ benchpots/preprocessing/__init__.py
@@ -7,10 +7,12 @@
 
 from .beijing_multisite_air_quality import preprocess_beijing_air_quality
 from .electricity_load_diagrams import preprocess_electricity_load_diagrams
+from .electricity_transformer_temperature import preprocess_ett
 from .physionet_2012 import preprocess_physionet2012
 
 __all__ = [
     "preprocess_physionet2012",
     "preprocess_beijing_air_quality",
     "preprocess_electricity_load_diagrams",
+    "preprocess_ett",
 ]
