5969613a70c8a3d4124e40d15ab04293b8856dbb
Wenjie Du <wenjay.du@gmail.com>
2024-05-24 23:50:19 +0800

    feat: add preprocess_beijing_air_quality;

--- benchpots/preprocessing/__init__.py
+++ benchpots/preprocessing/__init__.py
@@ -5,9 +5,10 @@
 # Created by Wenjie Du <wenjay.du@gmail.com>
 # License: BSD-3-Clause
 
+from .beijing_multisite_air_quality import preprocess_beijing_air_quality
 from .physionet_2012 import preprocess_physionet2012
-
 
 __all__ = [
     "preprocess_physionet2012",
+    "preprocess_beijing_air_quality",
 ]
