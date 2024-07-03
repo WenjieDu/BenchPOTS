be082384a0ddbc8172b0b62cf7ca0183826014dd
Wenjie Du <wenjay.du@gmail.com>
2024-07-03 10:10:30 +0800

    feat: add the preprocessing pipeline for the dataset Solar Alabama;

--- benchpots/datasets/__init__.py
+++ benchpots/datasets/__init__.py
@@ -13,6 +13,7 @@ from .pems_traffic import preprocess_pems_traffic
 from .physionet_2012 import preprocess_physionet2012
 from .physionet_2019 import preprocess_physionet2019
 from .ucr_uea_datasets import preprocess_ucr_uea_datasets
+from .solar_alabama import preprocess_solar_alabama
 
 __all__ = [
     "preprocess_physionet2012",
@@ -23,4 +24,5 @@ __all__ = [
     "preprocess_ett",
     "preprocess_pems_traffic",
     "preprocess_ucr_uea_datasets",
+    "preprocess_solar_alabama",
 ]
