f2f9baf566aba96128eb8d690c818ea72b3eecb2
Wenjie Du <wenjay.du@gmail.com>
2024-07-24 10:32:15 +0800

    refactor: config the project with pyproject.toml;

--- benchpots/__init__.py
+++ benchpots/__init__.py
@@ -5,28 +5,11 @@ BenchPOTS package
 # Created by Wenjie Du <wenjay.du@gmail.com>
 # License: BSD-3-Clause
 
-
-# BenchPOTS version
-#
-# PEP0440 compatible formatted version, see:
-# https://www.python.org/dev/peps/pep-0440/
-# Generic release markers:
-# X.Y
-# X.Y.Z # For bugfix releases
-#
-# Admissible pre-release markers:
-# X.YaN # Alpha release
-# X.YbN # Beta release
-# X.YrcN # Release Candidate
-# X.Y # Final release
-#
-# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
-# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
-__version__ = "0.2"
-
 from . import utils, datasets
+from .version import __version__
 
 __all__ = [
+    "__version__",
     "utils",
     "datasets",
 ]
