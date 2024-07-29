579e06f6bdf64b47468ad4ca90f4637a4f2b2757
Wenjie Du <wenjay.du@gmail.com>
2024-07-29 13:11:36 +0800

    refactor: sliding-window related functions;

--- benchpots/utils/sliding.py
+++ benchpots/utils/sliding.py
@@ -10,14 +10,16 @@ from typing import Union
 
 import numpy as np
 import torch
+
 from ..utils.logging import logger
 
 
 def sliding_window(
     time_series: Union[np.ndarray, torch.Tensor],
-    window_len: int,
-    sliding_len: int = None,
-) -> Union[np.ndarray, torch.Tensor]:
+    window_size: int,
+    stride: int,
+    drop_last: bool = True,
+) -> Union[np.ndarray, torch.Tensor, tuple]:
     """Generate time series samples with sliding window method, truncating windows from time-series data
     with a given sequence length.
 
@@ -31,11 +33,14 @@ def sliding_window(
     time_series :
         time series data, len(shape)=2, [total_length, feature_num]
 
-    window_len :
-        The length of the sliding window, i.e. the number of time steps in the generated data samples.
+    window_size :
+        Former window_len. The size of the window, i.e. the number of time steps in the generated data samples.
 
-    sliding_len :
-        The sliding length of the window for each moving step. It will be set as the same with n_steps if None.
+    stride :
+        Former sliding_len. The stride, i.e. the sliding length, of the window for each moving step.
+
+    drop_last :
+        Whether to drop the last sample if the left length is not enough for a complete window.
 
     Returns
     -------
@@ -43,23 +48,35 @@ def sliding_window(
         The generated time-series data samples of shape [seq_len//sliding_len, n_steps, n_features].
 
     """
-    sliding_len = window_len if sliding_len is None else sliding_len
-    total_len = time_series.shape[0]
-    start_indices = np.asarray(range(total_len // sliding_len)) * sliding_len
+    assert len(time_series) > window_size, (
+        f"time_series length {len(time_series)} is less than "
+        f"window_size {window_size}. There is no space for sliding."
+    )
+    assert (
+        stride > 0 and window_size > 0
+    ), f"stride {stride} and window_size {window_size} must be positive"
+    assert stride <= window_size, (
+        f"stride {stride} shouldn't be larger than window_size {window_size}. "
+        f"Otherwise there will be gaps between samples."
+    )
 
-    # remove the last one if left length is not enough
-    if total_len - start_indices[-1] < window_len:
+    stride = window_size if stride is None else stride
+    total_len = time_series.shape[0]
+    start_indices = np.asarray(range(total_len // stride)) * stride
+
+    # remove the last samples if left length is not enough
+    if total_len - start_indices[-1] < window_size:
         left_len = total_len - start_indices[-1]
-        to_drop = math.floor(window_len / sliding_len)
+        to_drop = math.floor(window_size / stride)
         logger.warning(
-            f"{total_len}-{start_indices[-1]}={left_len} < {window_len}. "
-            f"The last {to_drop} samples are dropped due to the left length {left_len} is not enough."
+            f"{total_len}-{start_indices[-1]}={left_len} < {window_size}. "
+            f"{to_drop} incomplete samples are dropped due to the left length {left_len} is not enough."
         )
         start_indices = start_indices[:-to_drop]
 
     sample_collector = []
     for idx in start_indices:
-        sample_collector.append(time_series[idx : idx + window_len])
+        sample_collector.append(time_series[idx : idx + window_size])
 
     if isinstance(time_series, torch.Tensor):
         samples = torch.cat(sample_collector, dim=0)
@@ -68,10 +85,15 @@ def sliding_window(
     else:
         raise RuntimeError
 
+    if not drop_last:
+        logger.info(
+            f"drop_last is set as False, the last sample is kept and will be returned independently."
+        )
+        return samples, time_series[start_indices[-1] + stride :]
     return samples
 
 
-def inverse_sliding_window(X, sliding_len):
+def inverse_sliding_window(X, stride):
     """Restore the original time-series data from the generated sliding window samples.
     Note that this is the inverse operation of the `sliding_window` function, but there is no guarantee that
     the restored data is the same as the original data considering that
@@ -86,8 +108,8 @@ def inverse_sliding_window(X, sliding_len):
         The generated time-series samples with sliding window method, shape of [n_samples, n_steps, n_features],
         where n_steps is the window size of the used sliding window method.
 
-    sliding_len :
-        The sliding length of the window for each moving step in the sliding window method used to generate X.
+    stride :
+        The stride (sliding length) of the window for each moving step in the sliding window method used to generate X.
 
     Returns
     -------
@@ -98,22 +120,20 @@ def inverse_sliding_window(X, sliding_len):
     assert len(X.shape) == 3, f"X should be a 3D array, but got {X.shape}"
     n_samples, window_size, n_features = X.shape
 
-    if sliding_len >= window_size:
-        if sliding_len > window_size:
+    if stride >= window_size:
+        if stride > window_size:
             logger.warning(
-                f"sliding_len {sliding_len} is larger than the window size {window_size}, "
+                f"sliding_len {stride} is larger than the window size {window_size}, "
                 f"hence there will be gaps between restored data."
             )
         restored_data = X.reshape(n_samples * window_size, n_features)
     else:
-        collector = [X[0][:sliding_len]]
-        overlap = X[0][sliding_len:]
+        collector = [X[0][:stride]]
+        overlap = X[0][stride:]
         for x in X[1:]:
-            overlap_avg = (overlap + x[:-sliding_len]) / 2
-            collector.append(overlap_avg[:sliding_len])
-            overlap = np.concatenate(
-                [overlap_avg[sliding_len:], x[-sliding_len:]], axis=0
-            )
+            overlap_avg = (overlap + x[:-stride]) / 2
+            collector.append(overlap_avg[:stride])
+            overlap = np.concatenate([overlap_avg[stride:], x[-stride:]], axis=0)
         collector.append(overlap)
         restored_data = np.concatenate(collector, axis=0)
     return restored_data
